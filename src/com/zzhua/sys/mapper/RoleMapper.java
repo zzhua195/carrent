b482f7d6206810539aa4efb2172d0d73a568db7c
zzhua <1255112011@qq.com>
2020-02-21 17:15:43 +0800

    角色菜单完成

---
+++ src/com/zzhua/sys/mapper/RoleMapper.java
@@ -0,0 +1,27 @@
+package com.zzhua.sys.mapper;
+
+import com.zzhua.sys.domain.Role;
+import com.zzhua.sys.vo.RoleVo;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
+
+public interface RoleMapper {
+    int deleteByPrimaryKey(Integer roleid);
+
+    int insert(Role record);
+
+    int insertSelective(Role record);
+
+    Role selectByPrimaryKey(Integer roleid);
+
+    int updateByPrimaryKeySelective(Role record);
+
+    int updateByPrimaryKey(Role record);
+
+    List<Role> queryAllRole(RoleVo roleVo);
+
+    void deleteRoleMenuByRid(Integer roleid);
+
+    void addMenuToRole(@Param("roleid") Integer roleid, @Param("mid") Integer mid);
+}
